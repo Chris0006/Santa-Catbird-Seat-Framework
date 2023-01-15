ef6bd7a3dde40f15bade21802d4d679cd1cd3aa3
Chris <83936866+Chris0006@users.noreply.github.com>
2023-01-15 11:28:49 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+print(
+"""
+===============================================================================
+                    [SantaShell]~[Santa v1.3.1 Catbird Seat]                                        
+===============================================================================
+      [Web]: https://netbeltfinance.com | [Twitter]: @xChris
+===============================================================================
+
+Available Tools:
+
+  0)  SantaShell Newbie - Demonstration for beginners
+
+  1)  Manager - Control your bots
+  2)  Builder - Generate a backdoor
+  3)  Listener - Make or setup a listener
+
+  4)  Controller - Configure the Manager
+  5)  WebScanner - Scan the web
+  6)  GUI - Graphical User Interface
+
+  7)  Exploits - Search for exploits
+  8)  Encoders - Encrypt a file
+  9)  Payloads - Search for a payload
+
+Available Commands:
+
+  exit      Completely exit
+  tools     Information on a specific tool
+  help      Show help menu
+  use       Use a specific tool
+"""
+)
